ffa2fffa8fa19701326bb92573f95cac54cc3772
Hiren Kalariya <66909260+hiren-kalariya@users.noreply.github.com>
2023-07-26 12:47:24 +0530

    slack boot url add

--- utils/slackBoat.js
+++ utils/slackBoat.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 exports.responseInClientSlack = async (body) => {
   try {
     return await axios.post(
-      //   `https://hooks.slack.com/services/T02G8MF7NT0/B052L30C4RW/bLXx9huFSGkJdhr6KqCE`,--->use slack webhook id
+      `https://hooks.slack.com/services/REPLACE/WITH/ACTUAL_WEBHOOK`,
       body
     );
   } catch (error) {

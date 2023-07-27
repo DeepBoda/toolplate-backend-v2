38942e7c83aeb4e27311d72be5165f0cdd2b84d8
DeepBoda <deepboda18@gmail.com>
2023-07-27 19:30:47 +0530

    generate profile image initials upluad and get in s3

---
+++ middlewares/generateProfile.js
@@ -0,0 +1,93 @@
+const md5 = require("md5");
+const { S3 } = require("@aws-sdk/client-s3");
+const sharp = require("sharp");
+
+const s3 = new S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  Bucket: process.env.BUCKET,
+  region: process.env.AWS_REGION,
+});
+
+// Function to generate a random background color
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+exports.generateProfilePic = async (username) => {
+  try {
+    let imageText = "";
+
+    if (username.includes(" ")) {
+      // If the username contains spaces (full name), use the first character from each word
+      const words = username.split(" ");
+      imageText = words.map((word) => word[0].toUpperCase()).join("");
+    } else {
+      // If the username is a single word, use the first character only
+      imageText = username[0].toUpperCase();
+    }
+
+    // Create a colored background image with initials
+    const imageWidth = 500;
+    const imageHeight = 500;
+    const backgroundColor = getRandomColor(); // Random background color
+    const fontSize = 250;
+
+    const image = await sharp({
+      create: {
+        width: imageWidth,
+        height: imageHeight,
+        channels: 4, // Set channels to 4 for RGBA (includes transparency)
+        background: { r: 0, g: 0, b: 0, alpha: 0 }, // Set initial background as transparent
+      },
+    })
+      .composite([
+        {
+          input: Buffer.from(
+            `<svg xmlns="http://www.w3.org/2000/svg" width="${imageWidth}" height="${imageHeight}">
+              <rect width="100%" height="100%" fill="${backgroundColor}"/>
+              <text
+                x="50%"
+                y="65%"
+                dominant-baseline="middle"
+                text-anchor="middle"
+                font-family="Arial"
+                font-size="${fontSize}px"
+                fill="#ffffff"
+              >
+                ${imageText}
+              </text>
+            </svg>`
+          ),
+        },
+      ])
+      .toFormat("webp") // Convert the image to WEBP format
+      .toBuffer();
+
+    // Upload the image to Amazon S3 with the correct Content-Disposition header
+    const uploadParams = {
+      Bucket: process.env.BUCKET,
+      Key: `profile_pics/${md5(username)}.webp`, // Use the hashed username as the filename with .webp extension
+      Body: image,
+      ContentType: "image/webp", // Set the Content-Type as image/webp for WEBP format
+      ContentDisposition: "inline", // Set the Content-Disposition header to "inline" to display the image in the browser
+    };
+
+    // Upload the image and handle success without any data response
+    await s3.putObject(uploadParams);
+
+    // Construct the URL for the uploaded image
+    const imageURL = `${process.env.BUCKET_URL}/${uploadParams.Key}`;
+    console.log(imageURL); // Check the value of imageURL
+
+    return imageURL;
+  } catch (error) {
+    console.error("Error uploading the image to S3:", error);
+    throw error;
+  }
+};
--- modules/user/controller.js
+++ modules/user/controller.js
@@ -1,76 +1,121 @@
 "use strict";
 
-// const { Op, literal } = require("sequelize");
 const createError = require("http-errors");
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const service = require("./service");
-const { deleteFilesFromS3 } = require("../../middlewares/multer");
+const admin = require("../../config/firebaseConfig"); // Firebase Admin SDK instance
+const { generateProfilePic } = require("../../middlewares/generateProfile");
 
-const { cl, jwtDecoder } = require("../../utils/service");
-const { usersqquery, sqquery } = require("../../utils/query");
-
+// Signup route
 exports.signup = async (req, res, next) => {
   try {
-    if (req.file) req.body.profilePic = req.file.location;
-    const user = await service.create(req.body);
+    const { username, email, password, FCM } = req.body;
 
+    // Check if the email already exists in Firebase Authentication
+    let firebaseUser;
+    try {
+      firebaseUser = await admin.auth().getUserByEmail(email);
+    } catch (error) {
+      // Ignore the error, assume email doesn't exist in Firebase
+      firebaseUser = null;
+    }
+
+    // Check if the email already exists in the local database
+    const existingUser = await service.findOne({ where: { email } });
+
+    if (firebaseUser || existingUser) {
+      throw createError(400, `Email ${email} already registered`);
+    }
+
+    // Generate the profile picture URL using the username
+    let profilePicUrl;
+    try {
+      profilePicUrl = await generateProfilePic(username);
+    } catch (error) {
+      // Handle error during profile picture generation (optional)
+      console.error("Error generating profile picture:", error);
+      // You can provide a default profile picture URL or handle the error gracefully
+      profilePicUrl = "YOUR_DEFAULT_PROFILE_PIC_URL";
+    }
+
+    // Create the user in Firebase Authentication
+    const userRecord = await admin.auth().createUser({
+      email,
+      password,
+      displayName: username,
+    });
+
+    // Get the user's UUID from Firebase
+    const uidFromFirebase = userRecord.uid;
+
+    // Create the user in your local database and store the FCM token and profilePicUrl
+    const user = await service.create({
+      username,
+      email,
+      password,
+      uid: uidFromFirebase,
+      FCM,
+      profilePic: profilePicUrl, // Store the generated profile picture URL in your local database
+    });
+
+    // Generate JWT token and send response
     const token = jwt.sign(
       {
-        ...user.toJSON(),
+        id: user.id,
         role: "User",
       },
-      process.env.JWT_SECRET
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIREIN }
     );
 
     res.status(200).json({
       status: "success",
+      message: "Signup successful",
       data: user,
       token,
     });
-  } catch (err) {
-    cl(err);
-    next(err);
+  } catch (error) {
+    console.log(error);
+    next(error);
   }
 };
 
+// Login route
 exports.login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
 
+    // Find the user in your local database using the email
     const user = await service.findOne({ where: { email } });
 
-    if (user) {
-      const correctPassword = await bcryptjs.compare(password, user.password);
-
-      if (correctPassword) {
-        const token = jwt.sign(
-          {
-            id: user.id,
-            role: "User",
-          },
-          process.env.JWT_SECRET,
-          { expiresIn: process.env.JWT_EXPIREIN }
-        );
-
-        res.status(200).json({
-          status: "success",
-          message: "Login successful",
-          token,
-          role: "User",
-        });
-      } else {
-        throw createError(
-          401,
-          "Login failed because Email and Password don't match"
-        );
-      }
-    } else {
-      throw createError(
-        401,
-        "Login failed because Email and Password don't match"
-      );
+    // If the user doesn't exist, return error
+    if (!user) {
+      throw createError(401, "Invalid email or password");
     }
+
+    // Check if the provided password matches the hashed password in the database
+    const correctPassword = await bcryptjs.compare(password, user.password);
+    if (!correctPassword) {
+      throw createError(401, "Invalid email or password");
+    }
+
+    // Generate JWT token and send response
+    const token = jwt.sign(
+      {
+        id: user.id,
+        role: "User",
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIREIN }
+    );
+
+    res.status(200).json({
+      status: "success",
+      message: "Login successful",
+      token,
+      role: "User",
+    });
   } catch (error) {
     next(error);
   }

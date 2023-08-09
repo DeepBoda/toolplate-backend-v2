efb8ae2d2636e5407b12ef925778e75985f2ad86
DeepBoda <deepboda18@gmail.com>
2023-08-09 20:06:20 +0530

    removed unnecessary routes of users

--- routes/user.js
+++ routes/user.js
@@ -1,28 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const { authMiddleware, protectRoute } = require("../middlewares/auth");
 
 router.use("/appConfig", require("../modules/appConfig"));
 router.use("/users", require("../modules/user"));
+router.use("/category", require("../modules/category"));
+router.use("/tag", require("../modules/tag"));
 router.use("/tool", require("../modules/tool"));
 router.use("/tools/rating", require("../modules/toolRating"));
 router.use("/toolLike", require("../modules/toolLike"));
-router.use("/toolTag", require("../modules/toolTag"));
-router.use("/toolCategory", require("../modules/toolCategory"));
-router.use("/toolView", require("../modules/toolView"));
 router.use("/toolWishlist", require("../modules/toolWishlist"));
 router.use("/blog", require("../modules/blog"));
-router.use("/category", require("../modules/category"));
-router.use("/blogCategory", require("../modules/blogCategory"));
-router.use("/tag", require("../modules/tag"));
-router.use("/blogTag", require("../modules/blogTag"));
-router.use("/blogView", require("../modules/blogView"));
 router.use("/blogLike", require("../modules/blogLike"));
 router.use("/comment", require("../modules/blogComment"));
 router.use("/blogWishlist", require("../modules/blogWishlist"));
 router.use("/commentLike", require("../modules/blogCommentLike"));
 router.use("/comments/reply", require("../modules/blogCommentReply"));
 router.use("/comments/replyLike", require("../modules/blogCommentReplyLike"));
-// router.use("/temp", require("../modules/temp"));
 
 module.exports = router;

0c688feeb632738d100f247e9af0d0ecad088614
DeepBoda <deepboda18@gmail.com>
2023-08-10 13:32:31 +0530

    seeder data

--- seeders/917-add-toolViews.js
+++ seeders/917-add-toolViews.js
@@ -3,17 +3,21 @@
 /** @type {import('sequelize-cli').Migration} */
 const { faker } = require("@faker-js/faker");
 const tools = require("../modules/tool/service");
+const users = require("../modules/user/service");
 module.exports = {
   async up(queryInterface, Sequelize) {
     const toolViews = [];
     const tool = await tools.findAll({
       attributes: ["id"],
     });
+    const user = await users.findAll({
+      attributes: ["id"],
+    });
 
     for (var i = 0; i < 1000; i++) {
       toolViews.push({
         toolId: faker.helpers.arrayElement(tool.map((el) => el.id)),
-        userId: 1,
+        userId: faker.helpers.arrayElement(user.map((el) => el.id)),
         createdAt: faker.date.between(
           "2020-01-01T00:00:00.000Z",
           "2023-03-01T00:00:00.000Z"

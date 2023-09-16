061dfce021604c89de4f8535c89e841f41e1db4c
DeepBoda <deepboda18@gmail.com>
2023-09-16 15:45:41 +0530

    fix search

--- utils/query.js
+++ utils/query.js
@@ -1,65 +1,39 @@
 const { Op } = require("sequelize");
 
-/**
- * Get the Sequelize operator for a given attribute and value.
- * @param {string} attribute - Operator attribute (e.g., 'gt', 'eq').
- * @param {any} value - Value to compare with.
- * @returns {Object|null} - Sequelize operator or null if invalid attribute.
- */
-exports.getOpAttributeValue = (attribute, value) => {
-  const operators = {
-    gt: Op.gt,
-    gte: Op.gte,
-    lt: Op.lt,
-    lte: Op.lte,
-    eq: Op.eq,
-    ne: Op.ne,
-    notBetween: Op.notBetween,
-    between: Op.between,
-    in: Op.in,
-    notIn: Op.notIn,
-  };
+const operators = {
+  gt: Op.gt,
+  gte: Op.gte,
+  lt: Op.lt,
+  lte: Op.lte,
+  eq: Op.eq,
+  ne: Op.ne,
+  notBetween: Op.notBetween,
+  between: Op.between,
+  in: Op.in,
+  notIn: Op.notIn,
+};
 
+exports.getOpAttributeValue = (attribute, value) => {
   return operators[attribute] ? { [operators[attribute]]: value } : null;
 };
 
-/**
- * Build a Sequelize query object for filtering and pagination.
- * @param {Object} q - Query parameters.
- * @param {Object} filter - Additional filter criteria.
- * @param {Array} searchFrom - Columns to search.
- * @param {Array} excludeColumnsFromOrder - Columns to exclude from sorting.
- * @param {Array} excludeFields - Fields to exclude from the query.
- * @returns {Object} - Sequelize query object.
- */
 exports.sqquery = (
-  q,
+  {
+    limit = 10000,
+    page = 1,
+    sort = "createdAt",
+    sortBy = "DESC",
+    search = "",
+    ...q
+  },
   filter,
   searchFrom = [],
   excludeColumnsFromOrder = [],
-  excludeFields = []
+  excludeFields = ["page", "sort", "limit", "fields", "sortBy", "search"]
 ) => {
-  // Parse and validate limit and page parameters with a maximum limit
-  const maxLimit = 1000; // Define your maximum limit
-  const limit = Math.min(parseInt(q.limit, 10) || 10000, maxLimit);
-  const page = parseInt(q.page, 10) || 1;
-
-  // Validate and set valid sort column and order
-  const validSortColumns = ["createdAt", "updatedAt", "otherColumn"]; // Define valid sort columns
-  const defaultSort = "createdAt";
-  const defaultSortBy = "DESC";
-
-  const sort = validSortColumns.includes(q.sort) ? q.sort : defaultSort;
-  const sortBy = q.sortBy || defaultSortBy;
-
-  // Extract and exclude specific fields from the query parameters
-  excludeFields.push("page", "sort", "limit", "fields", "sortBy", "search");
   excludeFields.forEach((el) => delete q[el]);
-
-  // Initialize the 'where' filter with additional filter criteria
   let where = { ...filter };
 
-  // Handle query parameters, including operators
   Object.keys(q).forEach((v) => {
     if (typeof q[v] === "object") {
       Object.keys(q[v]).forEach((e) => {
@@ -71,8 +45,6 @@ exports.sqquery = (
     }
   });
 
-  // Handle text-based search if provided
-  const search = q?.search || "";
   if (search && searchFrom.length) {
     const searchData = searchFrom.map((columnName) => ({
       [columnName]: {
@@ -80,14 +52,11 @@ exports.sqquery = (
       },
     }));
 
-    if (Object.keys(where).length) {
-      where = { ...where, [Op.or]: searchData };
-    } else {
-      where = { [Op.or]: searchData };
-    }
+    where = Object.keys(where).length
+      ? { ...where, [Op.or]: searchData }
+      : { [Op.or]: searchData };
   }
 
-  // Exclude specific columns from sorting if needed
   if (excludeColumnsFromOrder.includes(sort)) {
     return { where, limit, offset: (page - 1) * limit };
   }
@@ -95,21 +64,11 @@ exports.sqquery = (
   return { where, order: [[sort, sortBy]], limit, offset: (page - 1) * limit };
 };
 
-/**
- * Build a Sequelize query object for user-related queries.
- * @param {Object} q - Query parameters.
- * @returns {Object} - Sequelize query object for user-related queries.
- */
-exports.usersqquery = (q) => {
-  const limit = parseInt(q?.limit) || 10000;
-  const page = parseInt(q?.page) || 1;
-  const skip = (page - 1) * limit;
-  const sort = q?.sort || "createdAt";
-  const sortBy = q?.sortBy || "DESC";
-
-  if (q?.limit) {
-    return { order: [[sort, sortBy]], limit, offset: skip };
-  }
-
-  return { order: [[sort, sortBy]] };
+exports.usersqquery = ({
+  limit = 10000,
+  page = 1,
+  sort = "createdAt",
+  sortBy = "DESC",
+}) => {
+  return { order: [[sort, sortBy]], limit, offset: (page - 1) * limit };
 };

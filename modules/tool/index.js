ae7f2eb8f2064077c95422f1a8a3f131d00ec7bd
Hiren Kalariya <66909260+hiren-kalariya@users.noreply.github.com>
2023-09-23 13:16:49 +0530

    undo search router

--- modules/tool/index.js
+++ modules/tool/index.js
@@ -6,8 +6,8 @@ const {authMiddleware,protectRoute} = require("../../middlewares/auth");
 
 router.use( authMiddleware);
 router.route("/").get(authMiddleware, tool.getAll);
+router.route("/search").get(tool.search);
 router.route("/:slug").get(tool.getBySlug);
-router.route("/home/search").get(tool.search);
 router.route("/related/:id").get(tool.getRelatedTools);
 router.route("/data/:slug").get(tool.getDynamicBySlug);
 

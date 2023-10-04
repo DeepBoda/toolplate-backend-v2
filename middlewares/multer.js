f2507b837564156aaea83bbc93e18220cf1dfde4
DeepBoda <deepboda18@gmail.com>
2023-10-04 19:57:52 +0530

    optimise multer

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -2,6 +2,7 @@ const multerS3 = require("multer-s3");
 const multer = require("multer");
 const path = require("path");
 const { s3 } = require("../config/aws");
+const { v4: uuidv4 } = require("uuid");
 
 const upload = multer({
   storage: multerS3({
@@ -16,8 +17,7 @@ const upload = multer({
       });
     },
     key: function (req, file, cb) {
-      const uniqueFileName =
-        Date.now().toString() + "-" + Math.round(Math.random() * 1e9);
+      const uniqueFileName = `${Date.now()}-${uuidv4()}`;
       cb(null, uniqueFileName + path.extname(file.originalname));
     },
     contentType: multerS3.AUTO_CONTENT_TYPE, // Automatically detect content type based on file extension
@@ -27,9 +27,11 @@ const upload = multer({
 const deleteFilesFromS3 = (urls) => {
   try {
     const keys = urls.map((url) => url.split(`${process.env.BUCKET_URL}/`)[1]);
+
     if (keys.length === 0) {
       return; // No files to delete, return early
     }
+
     const payload = {
       Bucket: process.env.BUCKET,
       Delete: {
@@ -37,9 +39,11 @@ const deleteFilesFromS3 = (urls) => {
         Quiet: true,
       },
     };
+
     s3.deleteObjects(payload, (err, data) => {
       if (err) {
         console.error("Error deleting objects:", err);
+        throw err; // Throw the error to be caught and handled by the caller
       } else {
         console.log("Files deleted successfully", data.Deleted);
       }

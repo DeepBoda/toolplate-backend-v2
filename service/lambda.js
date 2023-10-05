e19eda6dd589d58d881a88e2c63138e6001a33ff
Hiren Kalariya <66909260+hiren-kalariya@users.noreply.github.com>
2023-10-05 17:06:02 +0530

    add: lamda push notificationn using topic add

---
+++ service/lambda.js
@@ -0,0 +1,27 @@
+"use strict";
+const AWS = require("aws-sdk");
+
+AWS.config.update({
+  accessKeyId: "",
+  secretAccessKey: "",
+  region: "",
+});
+const lambda = new AWS.Lambda();
+
+// Call the Lambda function
+const params = {
+  FunctionName: "sendNotificationToTopic",
+  Payload: JSON.stringify({
+    title: "Test Notification Title",
+    body: "This is a test notification body",
+    topic: "toolplate-blogs-dev",
+  }),
+};
+
+lambda.invoke(params, (err, data) => {
+  if (err) {
+    console.error("Error calling Lambda function:", err);
+  } else {
+    console.log("Notification send successfully");
+  }
+});

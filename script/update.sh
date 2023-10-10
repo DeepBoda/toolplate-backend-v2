1c6937c65c744f0cb072e574377155670ace2d52
DeepBoda <deepboda18@gmail.com>
2023-10-10 15:34:00 +0530

    Update: script.sh

--- script/update.sh
+++ script/update.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# Set appropriate permissions for the application directory (replace with your directory)
+sudo chmod -R 777 /root/toolplate-backend
+
+# Grant execute permissions to the script
+chmod +x update.sh
+
 # Change to the application directory
 cd /root/toolplate-backend
-
-# Update the codebase from your repository
-git restore .
-git pull
 
 # Install dependencies
 npm install

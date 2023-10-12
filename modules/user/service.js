58d2ae5c181219ff649034394d8f50fe8dffcbc0
DeepBoda <deepboda18@gmail.com>
2023-10-12 14:07:40 +0530

    search in notification

--- modules/user/service.js
+++ modules/user/service.js
@@ -5,26 +5,48 @@ exports.create = async (data) => {
   return Model.create(data);
 };
 
+exports.bulkCreate = async (data) => {
+  return Model.bulkCreate(data);
+};
+
 exports.findOne = async (conditions) => {
-  return Model.findOne(conditions);
+  return Model.findOne({ where: conditions });
 };
+
 exports.findAll = async (conditions) => {
-  return Model.findAll(conditions);
+  return Model.findAll({ where: conditions });
 };
+
 exports.findAndCountAll = async (conditions) => {
-  return Model.findAndCountAll(conditions);
+  return Model.findAndCountAll({ where: conditions });
 };
 
 exports.update = async (data, conditions) => {
-  return Model.update(data, conditions);
+  return Model.update(data, { where: conditions });
 };
 
 exports.delete = async (conditions) => {
-  return Model.destroy(conditions);
+  return Model.destroy({ where: conditions });
 };
 
 exports.count = async (conditions) => {
-  return Model.count(conditions);
+  return Model.count({ where: conditions });
+};
+
+exports.findOrCreate = async (data) => {
+  return Model.findOrCreate(data);
+};
+
+exports.updateOrCreate = async (data) => {
+  return Model.upsert(data);
+};
+
+exports.findOneAndUpdate = async (data, conditions) => {
+  const record = await Model.findOne(conditions);
+  if (record) {
+    return record.update(data);
+  }
+  return null;
 };
 
 exports.verifyReferral = async (referredCode) => {

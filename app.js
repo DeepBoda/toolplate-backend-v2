1cc56c18b06feeb0322581c30a0e260277e28aab
DeepBoda <deepboda18@gmail.com>
2023-10-23 12:13:19 +0530

    allow ipv4

--- app.js
+++ app.js
@@ -68,8 +68,8 @@ app.use(
 // Define your IP whitelist based on the environment
 const allowedIPs = isProduction
   ? ["13.126.237.126", "13.235.186.84"]
-  : ["15.207.242.14"];
-// : ["0.0.0.0", "::1", "15.207.242.14"];
+  : // : ["15.207.242.14"];
+    ["0.0.0.0", "::1", "15.207.242.14"];
 
 // Middleware for checking allowed IPs
 app.set("trust proxy", true);

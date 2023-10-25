cbf72e831babd102dbd97c488d6540f19ed4a545
DeepBoda <deepboda18@gmail.com>
2023-10-25 11:49:17 +0530

    remove restriction

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const { validateAPIKey } = require("../middlewares/auth");
 const router = express.Router();
 
-router.use("/api/v1/admin",validateAPIKey, require("./admin"));
-router.use("/api/v1",validateAPIKey, require("./user"));
+// router.use("/api/v1/admin",validateAPIKey, require("./admin"));
+// router.use("/api/v1",validateAPIKey, require("./user"));
+router.use("/api/v1/admin", require("./admin"));
+router.use("/api/v1", require("./user"));
 
 module.exports = router;

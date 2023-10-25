aee2f69685fd5fc06f56ac098f2acdb71362b9e2
DeepBoda <deepboda18@gmail.com>
2023-10-25 16:52:35 +0530

    bansi ip

--- app.js
+++ app.js
@@ -72,21 +72,21 @@ app.use(validateAPIKey);
 // Define your IP whitelist based on the environment
 const allowedIPs = isProduction
   ? ["13.126.237.126", "13.235.186.84"]
-  : ["0.0.0.0", "::1", "15.207.242.14", "localhost"];
+  : ["110.224.131.55", "15.207.242.14"];
 // : ["15.207.242.14"];
 
 // Middleware for checking allowed IPs
-// app.set("trust proxy", true);
-// app.use((req, res, next) => {
-//   const clientIP = req.ip; // Get the client's IP address
-//   // console.log("req : ", req);
-//   console.log("client IP: ", clientIP);
-//   if (allowedIPs.includes(clientIP)) {
-//     next(); // Allow the request to proceed to the next middleware
-//   } else {
-//     res.status(403).send("Access denied. Your IP is not whitelisted.");
-//   }
-// });
+app.set("trust proxy", true);
+app.use((req, res, next) => {
+  const clientIP = req.ip; // Get the client's IP address
+  // console.log("req : ", req);
+  console.log("client IP: ", clientIP);
+  if (allowedIPs.includes(clientIP)) {
+    next(); // Allow the request to proceed to the next middleware
+  } else {
+    res.status(403).send("Access denied. Your IP is not whitelisted.");
+  }
+});
 
 // Define your routes
 const indexRouter = require("./routes");

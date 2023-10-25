b2e6b613a911ae82c229e5f4c9757baef2192d46
DeepBoda <deepboda18@gmail.com>
2023-10-25 11:50:39 +0530

    log

--- app.js
+++ app.js
@@ -75,7 +75,10 @@ const allowedIPs = isProduction
 app.set("trust proxy", true);
 function checkAllowedIP(req, res, next) {
   const clientIP = req.ip; // Get the client's IP address
-  console.log("User IP: ", req.ip);
+  const IPs = req.ips; // Get the client's IP address
+  console.log("request : ", req);
+  console.log("User IP : ", req.ip);
+  console.log("IPs : ", req.ips);
   if (allowedIPs.includes(clientIP)) {
     next(); // Allow the request to proceed to the next middleware
   } else {

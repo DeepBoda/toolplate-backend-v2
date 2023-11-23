6caba27630cff5352480e958995f218b356f60be
DeepBoda <deepboda18@gmail.com>
2023-11-23 16:33:53 +0530

    fix rate limit

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -5,19 +5,26 @@ const createError = require("http-errors");
 
 exports.limiter = rateLimit({
   // Rate limiter configuration
-  windowMs: 15 * 60 * 1000, // 15 minute
-  max: 5, // Allow 5 requests per 15 minute
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // Allow 5 requests per 15 minutes
 
   // Redis store configuration
   store: new RedisStore({
     sendCommand: (...args) => client.sendCommand(args),
   }),
-
-  // Custom message for rate limit exceeded
-  message: (req, res, next) => {
-    next(createError(403, `Too many requests. Try again after 15 minutes.`));
+  headers: {
+    "Retry-After": 60,
+  },
+  message: (req, res) => {
+    res.status(403).json({
+      status: "error",
+      message: "Too many requests. Try again after 15 minutes.",
+    });
   },
 
+  skip: (req, res) => {
+    return req.ip === "127.0.0.1";
+  },
   keyGenerator: (req) => {
     // Use a combination of IP and API route for key generation
     return `${req.ip}:${req.originalUrl}`;

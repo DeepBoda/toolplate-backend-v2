595fafe2b636b52a2f13ac8f5f5a4977534be3a9
DeepBoda <deepboda18@gmail.com>
2023-12-02 12:00:50 +0530

    firebase

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const credentials = require("./toolplate-28af5-firebase-adminsdk-x2s3a-dfecc50c6a.json");
+const credentials = require("./toolplate-28af5-firebase-adminsdk-x2s3a-41e579504f.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(credentials),

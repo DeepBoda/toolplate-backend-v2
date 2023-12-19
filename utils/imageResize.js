86bb61ac51847b43be5b5e09c96bd052803a41e8
DeepBoda <deepboda18@gmail.com>
2023-12-19 14:59:35 +0530

    file upload api - admin

--- utils/imageResize.js
+++ utils/imageResize.js
@@ -1,6 +1,7 @@
 const AWS = require("@aws-sdk/client-s3");
 const sharp = require("sharp");
 require("dotenv").config();
+const { v4: uuidv4 } = require("uuid");
 const axios = require("axios");
 const s3Client = new AWS.S3({
   region: process.env.AWS_REGION,
@@ -121,6 +122,56 @@ exports.resizeAndUploadWebP = async (sizes, originalImageS3Link, keyPrefix) => {
     return true;
   } catch (err) {
     console.error("Error resizing and uploading WebP images", err);
+    return false;
+  }
+};
+
+exports.convertToAvifAndUpload = async (file) => {
+  try {
+    // Fetch the original image from the provided S3 link (assuming it's a publicly accessible URL)
+    const response = await axios.get(file.location, {
+      responseType: "arraybuffer",
+    });
+    const originalImageBuffer = Buffer.from(response.data, "binary");
+
+    const pipeline = sharp(originalImageBuffer)
+      .avif({
+        quality: 90,
+        lossless: false, // Consider using lossy compression for smaller file sizes
+        speed: 6, // Trade-off between compression speed and efficiency
+      })
+      .sharpen();
+
+    // Convert the image to AVIF format
+    const avifBuffer = await pipeline.toBuffer();
+
+    // Generate a unique file name for the S3 object
+    const uniqueFileName = `overview-${uuidv4()}`;
+
+    // Upload the AVIF image to S3
+    await s3Client.putObject({
+      Bucket: process.env.BUCKET,
+      Key: `${uniqueFileName}.avif`,
+      Body: avifBuffer,
+      ACL: "public-read",
+      ContentType: "image/avif",
+      Metadata: {
+        "Cache-Control": "public, max-age=31536000",
+        "Content-Disposition": `inline; filename="${uniqueFileName}.avif"`,
+      },
+    });
+
+    const cdn =
+      process.env.NODE_ENV === "production"
+        ? "https://cdn.toolplate.ai"
+        : "https://staging-cdn.toolplate.ai";
+
+    return {
+      originalUrl: file.location, // Update with the correct extension
+      avifUrl: `${cdn}/${uniqueFileName}.avif`,
+    };
+  } catch (err) {
+    console.error("Error converting and uploading image to AVIF", err);
     return false;
   }
 };

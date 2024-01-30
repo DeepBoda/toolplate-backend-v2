1ddf1e07328f012f02186cc4a0b12fdb6fa1c86e
DeepBoda <deepboda18@gmail.com>
2024-01-30 15:58:18 +0530

    fix

--- modules/toolSeo/script.js
+++ modules/toolSeo/script.js
@@ -2,24 +2,28 @@
 const service = require("./service");
 const toolService = require("../tool/service");
 const ToolCategory = require("../toolCategory/model");
+const Category = require("../category/model");
 
 const DataEntrySeo = async () => {
   try {
     const Tools = await toolService.findAll({
       include: {
         model: ToolCategory,
+        include: {
+          model: Category,
+        },
       },
     });
 
     for (let i in Tools) {
-      const title = `${Tools[i].title} AI - Key Features, Reviews, Pricing, & Alternative Tools`;
+      // const title = `${Tools[i].title} AI - Key Features, Reviews, Pricing, & Alternative Tools`;
       const categoryNames = Tools[i].toolCategories
-        .map((category) => category.name)
+        .map((c) => c.category.name)
         .join(" and ");
       const description = `Explore ${Tools[i].title} on Toolplate: a ${Tools[i].price} ${categoryNames} tool: Read in-depth features and details, user reviews, pricing, and find alternative tools of ${Tools[i].title}. Your one-stop resource for ${Tools[i].title} insights`;
 
       const payload = {
-        title: title,
+        // title: title,
         description: description,
       };
 

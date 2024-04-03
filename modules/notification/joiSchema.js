94f58533a74fe506ad34142cd6c04af665fea1be
DeepBoda <deepboda18@gmail.com>
2024-04-03 14:28:04 +0530

    notification update/delete

--- modules/notification/joiSchema.js
+++ modules/notification/joiSchema.js
@@ -11,8 +11,8 @@ module.exports = {
   update: Joi.object().keys({
     title: Joi.string(),
     body: Joi.string(),
-    topic: Joi.string(),
     click_action: Joi.string(),
     AdminId: Joi.number(),
+    schedule: Joi.date(),
   }),
 };

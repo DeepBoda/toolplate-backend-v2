a8bc5dfddc3e67fae2d91dac9c43f824cc7c169d
DeepBoda <deepboda18@gmail.com>
2024-06-04 17:16:11 +0530

    cercle ci yml

---
+++ .circleci/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# deploy.sh
+
+echo "Starting deployment..."
+# Add your deployment commands here
+# e.g., scp, rsync, or any other commands necessary for deployment
+echo "Deployment finished."

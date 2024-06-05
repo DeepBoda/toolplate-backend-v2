e13dcef9d92a3956185720217e6c119c3208abb0
DeepBoda <deepboda18@gmail.com>
2024-06-05 14:27:22 +0530

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-# deploy.sh
 
-echo "Starting deployment..."
-# Add your deployment commands here
-# e.g., scp, rsync, or any other commands necessary for deployment
-echo "Deployment finished."
+# Change to the application directory
+cd /root/toolplate-backend
+
+# Update the codebase from your repository
+git restore .
+git pull
+
+# Install dependencies
+npm install
+
+# Restart the application using PM2 (replace with your app name)
+pm2 restart "toolplate-backend"
